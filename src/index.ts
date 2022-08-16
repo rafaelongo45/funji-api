549fb8820ff4789d7d508ee1b882d9c240643414
Rafael Longo <rafaelongo45@gmail.com>
2022-08-16 14:50:56 -0300

    Adds cors library

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ import { errorHandler } from "./middlewares/handleError.js";
 
 dotenv.config();
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 app.use(router);
 app.use(errorHandler);
 
