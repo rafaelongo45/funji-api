2e6de0d88f9d5fa3b3816a022d6f9eed6a62ed23
Rafael Longo <rafaelongo45@gmail.com>
2022-08-16 21:14:22 -0300

    Fixed token generation and size of token

--- src/utils/tokenGenerator.ts
+++ src/utils/tokenGenerator.ts
@@ -7,6 +7,6 @@ dotenv.config();
 
 export async function generateToken(user: UserSignin){
   const JWT_KEY = process.env.JWT_KEY;
-  const token = jwt.sign(user, JWT_KEY);
+  const token = jwt.sign(user.email, JWT_KEY);
   return token;
 }
